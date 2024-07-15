2d5c68524b6ccb7a300d66ab36091d366f38bedf
muhmad <muhmad@live.no>
2024-07-15 11:02:44 +0200

    test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="lets-do-it",
+    name="ap98j3envoubi3fco1kc",
     version="0.0.71",
     packages=find_packages(),
     install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks", "tokenizers"],
