87f949e86b3199c143e50f67b0d83c876dd28b04
muhmad <muhmad@live.no>
2024-07-19 00:22:35 +0200

    test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.85",
+    version="0.0.90",
     packages=find_packages(),
     install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks", "tokenizers"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
