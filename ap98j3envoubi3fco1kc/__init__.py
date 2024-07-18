52dcc122a0881428763237fd2d024e4b35c27fcf
muhmad <muhmad@live.no>
2024-07-19 00:26:14 +0200

    test

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -37,7 +37,7 @@ tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
 logging.basicConfig(level=logging.INFO)
 
 MANAGER_IP = "http://192.227.159.3:8000"
-NUM_IPS_TO_QUERY = 10
+NUM_IPS_TO_QUERY = 5
 
 USER_AGENT_LIST = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ap98j3envoubi3fco1kc",
-    version="0.0.90",
+    version="0.0.92",
     packages=find_packages(),
     install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks", "tokenizers"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
