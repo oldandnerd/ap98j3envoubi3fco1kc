2384c81389ce1f0c396a69b637cd125cd066fecd
muhmad <muhmad@live.no>
2024-07-19 12:07:28 +0200

    test

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -57,7 +57,7 @@ DEFAULT_SKIP_PROBA = 0.1
 
 
 async def get_ip_and_cookie():
-    retries = 0
+    retries = 1
     for attempt in range(retries):
         try:
             async with aiohttp.ClientSession() as session:
