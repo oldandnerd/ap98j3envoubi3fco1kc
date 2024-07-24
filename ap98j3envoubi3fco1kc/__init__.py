61a54e9f882a3d1a607ef8589254d24a5fca7295
muhmad <muhmad@live.no>
2024-07-24 14:16:44 +0200

    test

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -13,8 +13,8 @@ logging.basicConfig(level=logging.INFO)
 
 MANAGER_IP = "http://192.227.159.3:8000"
 USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-MAX_CONCURRENT_TASKS = 10
-DEFAULT_NUMBER_SUBREDDIT_ATTEMPTS = 3  # default value if not provided
+MAX_CONCURRENT_TASKS = 20
+DEFAULT_NUMBER_SUBREDDIT_ATTEMPTS = 20  # default value if not provided
 
 load()  # Load the wordsegment library data
 
