b40e1b8785e6e31689e4593fba7728d842c7b564
muhmad <muhmad@live.no>
2024-08-01 12:21:24 +0200

    test

--- ap98j3envoubi3fco1kc/__init__.py
+++ ap98j3envoubi3fco1kc/__init__.py
@@ -920,7 +920,7 @@ async def query(parameters: Dict) -> AsyncGenerator[Item, None]:
     max_oldness_seconds = parameters.get('max_oldness_seconds')
     maximum_items_to_collect = parameters.get('maximum_items_to_collect', 25)  # Default to 25 if not provided
     min_post_length = parameters.get('min_post_length')
-    batch_size = parameters.get('batch_size', 1)
+    batch_size = parameters.get('batch_size', 50)
     nb_subreddit_attempts = parameters.get('nb_subreddit_attempts', 5)
     post_limit = parameters.get('post_limit', 100)  # Limit for the number of posts per subreddit
 
