a12ade6024ee870a31d1b0a8f07d9629c5cbc961
oldandnerd <muhmad.fauzi@gmail.com>
2024-08-25 14:25:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="ap98j3envoubi3fco1kc",
     version="0.0.93",
     packages=find_packages(),
-    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks", "tokenizers", "tiktoken"],
+    install_requires=["lxml>=4.9.3", "wordsegment==1.3.1", "exorde_data", "aiohttp", "aiohttp_socks", "tokenizers"],
     extras_require={"dev": ["pytest", "pytest-cov", "pytest-asyncio"]},
 )
